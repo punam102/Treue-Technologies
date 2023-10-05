4c4e3a5702be485a84789a711e1859664b314762
Punam Jagtap <99978788+punam102@users.noreply.github.com>
2023-10-05 23:07:39 +0530

    testing

---
+++ Task-3/unit.js
@@ -0,0 +1,24 @@
+function convert() {
+    const length = parseFloat(document.getElementById('length').value);
+    const unit = document.getElementById('unit').value;
+    let convertedLength;
+
+    switch (unit) {
+        case 'meters':
+            convertedLength = length * 100;
+            break;
+        case 'centimeters':
+            convertedLength = length;
+            break;
+        case 'kilometers':
+            convertedLength = length * 100000;
+            break;
+        case 'millimeters':
+            convertedLength = length * 0.1;
+            break;
+        default:
+            convertedLength = length;
+    }
+
+    document.getElementById('result').innerText = `Converted Length: ${convertedLength} centimeters`;
+}
