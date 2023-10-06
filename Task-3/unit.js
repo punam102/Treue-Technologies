963b43b60075bc6ecb778253e227cbcd5a081540
Punam Jagtap <99978788+punam102@users.noreply.github.com>
2023-10-06 14:55:22 +0530

    result showing perfectly

--- Task-3/unit.js
+++ Task-3/unit.js
@@ -10,15 +10,19 @@ function convert() {
       break;
     case "centimeters":
       convertedLength = length;
+      unitText = 'Centimeters';
       break;
     case "kilometers":
       convertedLength = length * 100000;
+      unitText = 'Kilometers';
       break;
     case "millimeters":
       convertedLength = length * 0.1;
+      unitText = 'Millimeters';
       break;
     default:
       convertedLength = length;
+      unitText = 'Unknown Unit';
   }
 
   document.getElementById('result').innerText = `Converted Length: ${convertedLength} ${unitText}
