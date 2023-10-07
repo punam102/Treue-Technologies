5757d714e79651cade210d2c1e8c5fc224b4fade
Punam Jagtap <99978788+punam102@users.noreply.github.com>
2023-10-07 15:01:18 +0530

    images added

--- Task-6/gallery.js
+++ Task-6/gallery.js
@@ -1,6 +1,48 @@
-lightbox.option({
-    resizeDuration: 200,
-    wrapAround: true,
-    disableScrolling: true,
-  });
-  
+let isLightboxOpen = false;
+let currentImageIndex = 0;
+
+function openLightbox() {
+  // Code to open the lightbox and display the image at currentImageIndex
+  isLightboxOpen = true;
+}
+
+function closeLightbox() {
+  // Code to close the lightbox
+  isLightboxOpen = false;
+}
+
+function navigateNext() {
+  // Code to navigate to the next image
+  if (currentImageIndex < images.length - 1) {
+    currentImageIndex++;
+    // Update lightbox to display the image at currentImageIndex
+  }
+}
+
+function navigatePrev() {
+  // Code to navigate to the previous image
+  if (currentImageIndex > 0) {
+    currentImageIndex--;
+    // Update lightbox to display the image at currentImageIndex
+  }
+}
+
+// Call these functions based on your UI interactions
+// For example, when clicking on an image thumbnail
+thumbnail.addEventListener('click', () => {
+  openLightbox();
+});
+
+// For the close button in the lightbox
+closeButton.addEventListener('click', () => {
+  closeLightbox();
+});
+
+// For the next and previous navigation buttons
+nextButton.addEventListener('click', () => {
+  navigateNext();
+});
+
+prevButton.addEventListener('click', () => {
+  navigatePrev();
+});
